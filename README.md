4b63c87e05787825cd22c5c8a2ca5eb1d47c9058
Costas Papastathis <papastathiscr@gmail.com>
2026-01-13 13:59:42 +0100

    Upgrading hugo (#91)
    * adding .util scripts
    
    * feat: adding script for building the bolg website with hugo
    
    * feat: Adding script for running the blog website locally
    
    * feat: Adding script for adding a new post
    
    * fix: excluding .bin folder from commiting it
    
    * fix: exluding commiting hugo_build.locl file
    
    * fix: Updating readme file
    
    * fix: upgrading hugo
    
    * fix: using build script to build public static assets
    
    * feat: adding updating tools action
    
    * feat: ading labels for tagging the error issues
    
    * fix: adding failure catch on deploy workflows
    
    * feat: adding workflow for synchronizing labels
    
    * feat: adding renovate
    
    * fix: replacing BUILDPACKDIR with ROOTDIR as it is a typo
    
    * fix: updating resource to css
    
    * renaming config.toml to hugo.toml

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 You can run the server locally to see your changes using the following command.
 
 ```
-hugo server -D
+./scripts/serve.sh
 ```
 
 ## Creating a new post
@@ -12,5 +12,5 @@ The existing blog posts can be found in the `/content/posts` directory. To add
 a new post, use the following command, and then modify the generated template.
 
 ```
-hugo new posts/0000-my-post.md
+./scripts/new_post.sh --name 0123-my-post
 ```
---
+++ scripts/build.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+readonly PROGDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly ROOTDIR="$(cd "${PROGDIR}/.." && pwd)"
+
+# shellcheck source=SCRIPTDIR/.util/tools.sh
+source "${PROGDIR}/.util/tools.sh"
+
+# shellcheck source=SCRIPTDIR/.util/print.sh
+source "${PROGDIR}/.util/print.sh"
+
+function main() {
+  local token
+  token=""
+
+  while [[ "${#}" != 0 ]]; do
+    case "${1}" in
+      --help|-h)
+        shift 1
+        usage
+        exit 0
+        ;;
+
+      --token|-t)
+        token="${2}"
+        shift 2
+        ;;
+
+      "")
+
+        shift 1
+        ;;
+
+      *)
+        util::print::error "unknown argument \"${1}\""
+    esac
+  done
+
+  tools::install "${token}"
+
+  util::print::title "Building Hugo site..."
+  "${ROOTDIR}/.bin/hugo"
+
+  util::print::success "Site built successfully!"
+}
+
+function usage() {
+  cat <<-USAGE
+build.sh [OPTIONS]
+
+Builds the Hugo site.
+
+OPTIONS
+  --help   -h         prints the command usage
+  --token  -t <token> Token used to download assets from GitHub (optional)
+USAGE
+}
+
+function tools::install() {
+  local token
+  token="${1}"
+
+  util::tools::hugo::install \
+    --directory "${ROOTDIR}/.bin" \
+    --token "${token}"
+}
+
+main "${@:-}"
+
