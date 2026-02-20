bfa0a81fc9e7b524ea76cc4d938d6cf1ac3d8e8a
Costas Papastathis <papastathiscr@gmail.com>
2026-02-20 21:45:34 +0100

    Fixing hugo installation on macos (#97)
    * Updating tools
    
    * chore: use homebrew if Hugo does not provide tarball
    * since v0.153.0, Hugo releases stopped shipping tarball for a bunch of OS/arch, including MacOS arm; but brew seems reliable.
    
    * bug: fix hugo warning about html rendering
    Because <none> looks like HTML, Hugo printed this warn message:
    WARN:  Raw HTML omitted while rendering "paketo-buildpacks/blog/content/posts/0038-paketo-buildpacks-2025-year-in-review.md"; see https://gohugo.io/getting-started/configuration-markup/#rendererunsafe
    You can suppress this warning by adding the following to your site configuration:
    ignoreLogs = ['warning-goldmark-raw-html']
    
    * Updating tools
    
    * Updating tools
    
    * fix: updates from code review
    
    * fix: adding update functionality
    
    * fix: using always the latest vresion in mac with homebrew
    
    ---------
    
    Co-authored-by: paketo-bot <paketobuildpacks@gmail.com>
    Co-authored-by: Anthony Dahanne <anthony.dahanne@gmail.com>

--- scripts/.util/tools.sh
+++ scripts/.util/tools.sh
@@ -61,6 +61,7 @@ function util::tools::path::export() {
 function util::tools::hugo::install() {
   local dir token
   token=""
+  do_not_update="false"
 
   while [[ "${#}" != 0 ]]; do
     case "${1}" in
@@ -74,6 +75,11 @@ function util::tools::hugo::install() {
         shift 2
         ;;
 
+      --do-not-update)
+        do_not_update="${2}"
+        shift 2
+        ;;
+
       *)
         util::print::error "unknown argument \"${1}\""
     esac
@@ -82,38 +88,75 @@ function util::tools::hugo::install() {
   mkdir -p "${dir}"
   util::tools::path::export "${dir}"
 
+  local version current_version
+
+  version="$(jq -r .hugo "$(dirname "${BASH_SOURCE[0]}")/tools.json")"
+  version="${version#v}"
+
+  if [[ -f "${dir}/hugo" ]]; then
+    current_version="$("${dir}/hugo" version 2>/dev/null | grep -oE "v[0-9]+\.[0-9]+\.[0-9]+" | head -1)"
+    current_version="${current_version#v}"
+  else
+    current_version=""
+  fi
+
+  if [[ "${current_version}" == "${version}" ]]; then
+    util::print::info "Using $("${dir}"/hugo version)"
+    return
+  fi
+
+  if [[ "${current_version}" != "${version}" && "${do_not_update}" == "true" && "${current_version}" != "" ]]; then
+    util::print::info "Skipping hugo update"
+    return
+  fi
+
+  if [[ "${current_version}" != "${version}"  && "${do_not_update}" == "false" ]]; then
+    util::print::info "Updating hugo to version:${version}"
+    rm -f "${dir}/hugo"
+  fi
+
   if [[ ! -f "${dir}/hugo" ]]; then
-    local version curl_args os arch
+    local curl_args os arch os_arch tarball_url
 
-    version="$(jq -r .hugo "$(dirname "${BASH_SOURCE[0]}")/tools.json")"
-
-    curl_args=(
-      "--fail"
-      "--silent"
-      "--location"
-    )
-
-    if [[ "${token}" != "" ]]; then
-      curl_args+=("--header" "Authorization: Token ${token}")
-    fi
-
-    util::print::title "Installing hugo ${version} (extended)"
+    util::print::title "Installing hugo version:${version}"
 
     os=$(util::tools::os)
     arch=$(util::tools::arch --format-amd64-64bit)
 
-    local version_without_v="${version#v}"
-    local os_arch="${os}-${arch}"
 
-    if [[ "${os}" == "darwin" ]]; then
-      os_arch="darwin-universal"
+    if [[ "${os}" == "linux" ]]; then
+      curl_args=(
+        "--fail"
+        "--silent"
+        "--location"
+      )
+
+      if [[ "${token}" != "" ]]; then
+        curl_args+=("--header" "Authorization: Token ${token}")
+      fi
+
+      os_arch="${os}-${arch}"
+      tarball_url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_extended_${version}_${os_arch}.tar.gz"
+      curl "${curl_args[@]}" "${tarball_url}" | tar xzf - -C "${dir}" hugo
+      chmod +x "${dir}/hugo"
+    elif [[ "${os}" == "darwin" ]]; then
+      local brew_prefix
+
+      if ! command -v brew >/dev/null 2>&1; then
+        util::print::error "Homebrew is required to install Hugo ${version} when the release tarball is unavailable"
+      fi
+
+      # we always install the latest version of hugo
+      # as brew does not support installing a specific version
+      brew install hugo
+
+      brew_prefix="$(brew --prefix hugo)"
+
+      ln -sf "${brew_prefix}/bin/hugo" "${dir}/hugo"
+    else
+      util::print::error "Unsupported OS for Hugo install: ${os}"
+      exit 1
     fi
-
-    curl "${curl_args[@]}" \
-      "https://github.com/gohugoio/hugo/releases/download/${version}/hugo_extended_${version_without_v}_${os_arch}.tar.gz" | \
-        tar xzf - -C "${dir}" hugo
-
-    chmod +x "${dir}/hugo"
   else
     util::print::info "Using $("${dir}"/hugo version)"
   fi
